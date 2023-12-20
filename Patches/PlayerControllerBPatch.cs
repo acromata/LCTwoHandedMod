770d99db687be1f9e8654b9f5929833d93490ece
acromata <87761538+acromata@users.noreply.github.com>
2023-12-20 20:36:36 +0000

    Add project files.

---
+++ Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,35 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoHanded.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("BeginGrabObject")]
+        [HarmonyPostfix]
+        static void UpdateBeginGrabObject(ref bool ___twoHanded)
+        {
+            ___twoHanded = false;
+        }
+
+        [HarmonyPatch("GrabObjectClientRpc")]
+        [HarmonyPostfix]
+        static void UpdateGrabObjectClientRpc(ref bool ___twoHanded)
+        {
+            ___twoHanded = false;
+        }
+
+        [HarmonyPatch("SwitchToItemSlot")]
+        [HarmonyPostfix]
+        static void UpdateSwitchToItemSlot(ref bool ___twoHanded)
+        {
+            ___twoHanded = false;
+        }
+    }
+}
---
+++ plugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using TwoHanded.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoHanded
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class TwoHandedBase : BaseUnityPlugin
+    {
+        // Mod Metadata
+        private const string modGUID = "Acromata.TwoHanded";
+        private const string modName = "Two Handed Mod";
+        private const string modVersion = "1.0.0";
+
+        // Mod Setup
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static TwoHandedBase Instance;
+        internal ManualLogSource logSource;
+
+        void Awake()
+        {
+            // Instance singleton
+            if(Instance == null)
+            {
+                Instance = this;
+            }
+
+            // Setup log
+            logSource = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            // Patches
+            harmony.PatchAll(typeof(TwoHandedBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
